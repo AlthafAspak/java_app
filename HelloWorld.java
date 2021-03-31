b7125cca36ad5ac5d5dbb0f571e3967d303eeb9f
EC2 Default User <ec2-user@ip-172-31-44-25.ap-south-1.compute.internal>
2021-03-31 18:07:47 +0000

    g

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello Althaf and Welcome Devops Environment"); 
     }
 }
